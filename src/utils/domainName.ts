b597157ec587e2bbd7118bad066d2fb7032ad19e
br14nn <vituallabrian727@gmail.com>
2024-02-19 23:24:46 +0800

    changed the domainName

--- src/utils/domainName.ts
+++ src/utils/domainName.ts
@@ -1,2 +1,4 @@
-const domainName = "http://localhost:3000";
+//https://simple-inventory-management-system.vercel.app
+//http://localhost:3000
+const domainName = "https://simple-inventory-management-system.vercel.app";
 export default domainName;
